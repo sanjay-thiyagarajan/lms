fa4317010cedf70ac3d39da3fe47eeb910287dd0
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2022-06-30 21:09:10 +0530

    Update base.py

--- LMS/settings/base.py
+++ LMS/settings/base.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY',default='INSECURE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
